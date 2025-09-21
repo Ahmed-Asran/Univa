538fdcf6eb98cca19aaa4c86b584d2692b8bd7bf
Ahmed Asran <ahmedasran.em@gmail.com>
2025-09-21 15:48:00 +0300

    FIX:edit exam or assignment dates

--- routes/api.php
+++ routes/api.php
@@ -36,6 +36,7 @@ Route::post('course-section-update/{id}',[CourseSectionControoler::class,'update
 Route::post('course-section-delete/{id}',[CourseSectionControoler::class,'destroy'])->middleware(['api','auth:sanctum','role:admin']);
 Route::get('course-sections-current',[CourseSectionControoler::class,'currentSections'])->middleware(['api','auth:sanctum','role:admin']);
 Route::post('courses/material-upload',[MaterialController::class,'uploadMaterial'])->middleware(['api','auth:sanctum','role:faculty']);
+Route::put('courses/material-update/{id}',[MaterialController::class,'updateAssignmentDate'])->middleware(['api','auth:sanctum','role:faculty']);
 Route::get('courses/materials/{sectionId}',[MaterialController::class,'getMaterials'])->middleware(['api','auth:sanctum','role:faculty,student']);
 Route::get('courses/material/{materialId}',[MaterialController::class,'getMaterial'])->middleware(['api','auth:sanctum','role:faculty,student']);
 Route::get('course/enrollments',[CreateEnrollmentController::class,'index'])->middleware(['api','auth:sanctum','role:student']);
